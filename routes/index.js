5962c9239f320e21735bc393d32cb2e9e8b47a02
cormactaylor <cormacmtaylor@gmail.com>
2024-11-16 17:46:58 -0500

    fixed the patched routing bug and started styling

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@
  * I pledge my honor that I have abided by the Stevens Honor System.
  */
 import fibRoutes from './fibonacci_prime.js';
+import {static as staticDir} from 'express';
 
 const constructorMethod = (app) => {
   app.use('', fibRoutes);
+  app.use('/public', staticDir('public'));
 
   app.use('*', (_, res) => {
     res.redirect('/');
