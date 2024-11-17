97762fa62744b4b3f8a6a016404b2d8e75da2f0f
cormactaylor <cormacmtaylor@gmail.com>
2024-11-16 20:22:03 -0500

    functionality done

--- public/js/main.js
+++ public/js/main.js
@@ -11,33 +11,44 @@ let form = document.getElementById("form");
 let fibInput = document.getElementById("fibonacci_index_input");
 let errorDiv = document.getElementById("error");
 let results = document.getElementById("fibonacciResults");
-let frmLabel = document.getElementById("formLabel");
+let formLabel = document.getElementById("formLabel");
 if (form) {
   form.addEventListener("submit", (event) => {
     event.preventDefault();
-    const input = input.value;
-    if (input && Number.isInteger(input)) {
-      const fibIndex = input < 1 ? 0 : input;
-      // TODO
-
-
-      textInput.classList.remove("inputClass");
+    const input = fibInput.value;
+    let index;
+    if (!isNaN(input) && Number.isInteger((index = Number.parseFloat(input)))) {
+      const fibIndex = index < 1 ? 0 : index;
       errorDiv.hidden = true;
-      frmLabel.classList.remove("error");
+      formLabel.className = "text";
+      fibValue = fib(fibIndex);
       let li = document.createElement("li");
-
-      li.innerHTML = textInput.value;
-      myUl.appendChild(li);
-      myForm.reset();
-      textInput.focus();
-
+      li.className = isPrime(fibValue) ? "is-prime" : "not-prime";
+      li.innerHTML = `The Fibonacci of ${input} is ${fibValue}.`;
+      results.appendChild(li);
+      form.reset();
+      fibInput.focus();
     } else {
-      textInput.value = "";
+      form.reset();
       errorDiv.hidden = false;
       errorDiv.innerHTML = "You must enter an integer value!";
-      frmLabel.className = "error";
-      textInput.focus();
-      textInput.className = "inputClass";
+      formLabel.className = "error";
+      fibInput.focus();
     }
   });
 }
+
+function fib(index) {
+  function fasterFib(a, b, i) {
+    if (i === 0) return a;
+    else return fasterFib(b, a + b, --i);
+  }
+  return fasterFib(0, 1, index);
+}
+
+const isPrime = (val) => {
+  if (val < 2) return false;
+  const sqrtval = Math.floor(Math.sqrt(val));
+  for (let i = 2; i <= sqrtval; i++) if (val % i === 0) return false;
+  return true;
+};
